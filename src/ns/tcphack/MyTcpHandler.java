215f609246ad7c4234b6226ddacb6e8731560432
Live session user <jjkramok@gmail.com>
2015-03-19 11:32:30 +0000

    added default method

--- src/ns/tcphack/MyTcpHandler.java
+++ src/ns/tcphack/MyTcpHandler.java
@@ -1,13 +1,20 @@
 package ns.tcphack;
 
+import java.util.Arrays;
+
 class MyTcpHandler extends TcpHandler {
+	
+	public static final byte[] SRC = new byte[16]; //TODO add address
+	public static final byte[] DST = new byte[16]; //TODO add address
+	public static final byte[] HOP_LMT = new byte[]{0x1e}; //hop limit = 30
+	
 	public static void main(String[] args) {
 		new MyTcpHandler();
 	}
 
 	public MyTcpHandler() {
 		super();
-
+		
 		boolean done = false;
 		while (!done) {
 			// TODO: Implement your client for the server by combining:
@@ -23,11 +30,29 @@ class MyTcpHandler extends TcpHandler {
 			//           The data you'll receive and send will and should contain all packet 
 			//           data from the network layer and up.
 
-            byte[] dtout = [0110]; //Version 6
+            byte[] dtout = new byte[40]; //An outgoing packet, with size 40
+            this.addDefault(dtout); //outgoing now has addresses
+            
+            
+            
+            
             this.sendData(dtout);
             byte[] dtin = this.receiveData(1000);
-            System.out.println("incoming dataArray: " + Array.toString(dtin));
+            System.out.println("incoming dataArray: " + Arrays.toString(dtin));
 
 		}   
 	}
+	
+	/**
+	 * Adding src and dst addresses to a packet aswell as a hop limit
+	 * @param packet the packet in which the addresses will be written (length of 40 octets)
+	 * @return byte[] with src and dst addresses copied into
+	 */
+	public byte[] addDefault(byte[] packet) {
+		System.arraycopy(SRC, 0, packet, 8, SRC.length);
+		System.arraycopy(DST, 0, packet, 24, DST.length);
+		System.arraycopy(HOP_LMT, 0, packet, 7, HOP_LMT.length);
+		return packet;
+	}
+	
 }
